bf1d1d7475dd5618fe92e6ac923e3f7fa05dbcd5
tiago-rodrigues1 <tiagords127@gmail.com>
2022-08-26 18:34:57 -0300

    add b.v e b.vi

--- b-script.sql
+++ b-script.sql
@@ -1,3 +1,5 @@
+USE dogaodb;
+
 -- i
 INSERT INTO produtos (nome, valor) VALUES 
 ('Refrigerante Lata', 5),
@@ -31,3 +33,49 @@ INSERT INTO estados (descricao) VALUES
 ('Pedido Realizado'),
 ('Prato em Execução'),
 ('Pronto');
+
+-- v
+INSERT INTO clientes (nome, nascimento) VALUES 
+('Ana Maria Braga', '1949-04-01');
+
+INSERT INTO pedidos (cliente_id, estado_id, data_hora) VALUES
+(1, 2, '2022-08-26 17:50:00');
+
+INSERT INTO cachorro_quentes (pedidos_numero) VALUES
+(1),
+(1);
+
+INSERT INTO cachorro_quente_ingrediente (cachorro_quente_id, ingrediente_cod) VALUES
+/*1º cachorro_quente*/
+(1, 1),
+(1, 2),
+(1, 6),
+(1, 8),
+/*2º cachorro_quente*/
+(2, 3),
+(2, 7),
+(2, 9);
+
+INSERT INTO pedido_produto (pedido_numero, produto_cod, quantidade) VALUES
+(1, 1, 2),
+(1, 2, 1);
+
+-- vi
+INSERT INTO clientes (nome, nascimento) VALUES 
+('Raul Seixas', '1945-06-28');
+
+INSERT INTO pedidos (cliente_id, estado_id, data_hora) VALUES
+(2, 1, '2022-08-26 18:10:00');
+
+INSERT INTO cachorro_quentes (pedido_numero) VALUES 
+(3);
+
+INSERT INTO cachorro_quente_ingrediente (cachorro_quente_id, ingrediente_cod) VALUES
+(5, 4),
+(5, 3),
+(5, 5),
+(5, 8);
+
+INSERT INTO pedido_produto (pedido_numero, produto_cod, quantidade) VALUES
+(3, 4, 1),
+(3, 6, 1);
